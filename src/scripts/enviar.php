7b79cdc457005a7f11b662adb76d08eab647cc1f
Eduardo Lopes <edulopesresende19@gmail.com>
2026-08-24 17:44:29 -0300

    comentarios e assets

--- src/scripts/enviar.php
+++ src/scripts/enviar.php
@@ -2,8 +2,10 @@
 require_once __DIR__ . '/../../phpmailer/src/PHPMailer.php';
 require_once __DIR__ . '/../../phpmailer/src/SMTP.php';
 require_once __DIR__ . '/../../phpmailer/src/Exception.php';
+// Tenta carregar o arquivo .env, porém não consegue (nos meus testes pelo menos)
 require_once __DIR__ . '/../../.env';
 
+// Importa as classes do PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
@@ -31,18 +33,25 @@ if (empty($assunto) || empty($mensagem)) {
 $assunto = str_replace(["\r", "\n"], '', $assunto);
 $mensagemSegura = nl2br(htmlspecialchars($mensagem));
 
+/* Para testar a funcionalidade do envio de email será necessário criar uma senha de aplicativo no Gmail, 
+pois o Google não permite mais o uso de senhas normais para aplicativos de terceiros. Tendo que ativar 
+a verificação em duas etapas e criar a senha de aplicativo.
+E coloque seu email nas partes comentadas "Email que vai receber a mensagem" 
+
+LEMBRE-SE DE NÃO COMPARTILHAR ESSA SENHA OU COMITAR O CÓDIGO COM ELA
+*/
 try {
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;          
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'dudukakaroto4@gmail.com';
+    $mail->Username = 'dudukakaroto4@gmail.com'; // Email que vai receber a mensagem
     $mail->Password = ''; // Substitua pela senha de aplicato do seu email
     $mail->Port = 587;
 
-    $mail->setFrom('dudukakaroto4@gmail.com');
+    $mail->setFrom('dudukakaroto4@gmail.com'); // Email que vai receber a mensagem
      $mail->addReplyTo($emailRemetente, 'Visitante do site');
-    $mail->addAddress('dudukakaroto4@gmail.com');
+    $mail->addAddress('dudukakaroto4@gmail.com'); // Email que vai receber a mensagem
 
      $mail->isHTML(true);
     $mail->Subject = $assunto;
